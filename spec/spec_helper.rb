9df4eba6a791aa63afd763f9609524396c5f5a0c
Alex Saunders <alex@existent.co.uk>
2017-01-17 14:51:51 +0000

    removes commented out code from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,6 @@ require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
-  # config.before(:each) do
-  #   stub_request(:get, %r|https://api.wmata.com/StationPrediction.svc/json/GetPrediction/|).
-  #     to_return(status: 200, body: File.open("./spec/fixtures/next_trains.json"){|f| f.read}, headers: {})
-  # end
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
